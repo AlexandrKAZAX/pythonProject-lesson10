d4ad714e460c21f6f719327b48241294b1cf21f3
AlexandrKAZAX <101847966+AlexandrKAZAX@users.noreply.github.com>
2022-03-27 23:13:05 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from utils1 import *
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def page_main():
+    return get_candidate(all_candidates)
+
+
+@app.route("/candidate/<int:id>")
+def page_candidate(id):
+    return get_candidate_id(id, all_candidates)
+
+
+@app.route("/skills/<skill>")
+def page_skills(skill):
+    return get_candidate_skills(skill, all_candidates)
+
+
+app.run()
+
